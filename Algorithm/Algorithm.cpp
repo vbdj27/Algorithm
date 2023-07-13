647b9bb281f901d5751b947bb40699ec30f691be
vbdj27 <81547880+vbdj27@users.noreply.github.com>
2023-07-13 18:10:39 +0900

    binary search array

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -9,10 +9,42 @@
 
 using namespace std;
 
+vector<int> numbers;
 
+void BinarySearch(int N)
+{
+	int left = 0;
+	int right = numbers.size() -1 ;
+
+	while (left <= right)
+	{
+		cout << "탐색 범위:" << left << "~" << right <<endl;
+
+		int mid = (left + right) / 2;
+
+		if (N < numbers[mid])
+		{
+			cout << N << "<" << numbers[mid] << endl;
+			right = mid - 1;
+		}
+
+		else if (N > numbers[mid])
+		{
+			cout << N << ">" << numbers[mid] << endl;
+			left = mid + 1;
+		}
+
+		else
+		{
+			cout << "찾음" << endl;
+			break;
+		}
+	}
+}
 
 int main()
 {
-	
+	numbers = vector<int>{1, 8, 15, 23, 32, 44, 56, 63, 81, 91};
+	BinarySearch(82);
 }
 
