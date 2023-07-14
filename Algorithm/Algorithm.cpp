43076530263b7c5e7915e382682f3059d697bf2a
vbdj27 <81547880+vbdj27@users.noreply.github.com>
2023-07-14 14:47:34 +0900

    BinarySearchTree

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,50 +1,28 @@
-﻿// Algorithm.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿
 #include <iostream>
 #include <vector>
 #include <list>
 #include <stack>
 #include <queue>
+#include "BinarySearchTree.h"
 
 using namespace std;
 
-vector<int> numbers;
-
-void BinarySearch(int N)
-{
-	int left = 0;
-	int right = numbers.size() -1 ;
-
-	while (left <= right)
-	{
-		cout << "탐색 범위:" << left << "~" << right <<endl;
-
-		int mid = (left + right) / 2;
-
-		if (N < numbers[mid])
-		{
-			cout << N << "<" << numbers[mid] << endl;
-			right = mid - 1;
-		}
-
-		else if (N > numbers[mid])
-		{
-			cout << N << ">" << numbers[mid] << endl;
-			left = mid + 1;
-		}
-
-		else
-		{
-			cout << "찾음" << endl;
-			break;
-		}
-	}
-}
 
 int main()
 {
-	numbers = vector<int>{1, 8, 15, 23, 32, 44, 56, 63, 81, 91};
-	BinarySearch(82);
+	BinarySearchTree bst;
+
+	bst.Insert(20);
+	bst.Insert(10);
+	bst.Insert(30);
+	bst.Insert(25);
+	bst.Insert(26);
+	bst.Insert(40);
+	bst.Insert(50);
+	
+	bst.Delete(20);
+
+	bst.Print();
 }
 
