2ac9b5436fa3d2f7cdddf8724578a2ecad6554d1
vbdj27 <81547880+vbdj27@users.noreply.github.com>
2023-07-26 14:55:07 +0900

    LIS

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -10,39 +10,60 @@
 
 using namespace std;
 
-//memoization
-int cache[50][50];
+// LIS (Longest Increasing Sequence)
 
-int combination(int n, int r)
+// Seq : 1 9 2 5 7
+// 부분 수열 : 일부(0개 이상) 숫자를 지우고 남은 수열
+// ex) 1 2 5
+// ex) 1 9 5 7
+// 순 증가 부분 수열
+// ex) 1 2 5
+
+// LIS : 제일 긴 [순 증가 부분 수열]의 길이
+// 1 2 5 7 길이 = 4
+
+
+int cache[100];
+vector<int> seq;
+
+int Lis(int pos)
 {
-	//기저 사례
-	if (r == 0 || n == r)
+	// 기저 사항
+	/*if (pos == seq.size() - 1)
 	{
 		return 1;
-	}
+	}*/
 
-	// 이미 답을 구한 적이 있으면 바로 반환
-	int& ret = cache[n][r];
-	
+	// 캐시 확인
+	int& ret = cache[pos];
 	if (ret != -1)
 	{
 		return ret;
 	}
 
-	// 새로 답을 구해서 캐시에 저장
-	return ret = combination(n-1, r-1) + combination(n-1, r);
+	// 구현
+	ret = 1;
+
+	for (int next = pos + 1; next < seq.size(); next++)
+	{
+		if (seq[pos] < seq[next])
+		{
+			ret = max(ret, 1 + Lis(next));
+		}
+	}
+
+	return ret;
 }
-
-
 
 int main()
 {
 	::memset(cache, -1, sizeof(cache));
+	seq = vector<int>{10, 1, 9, 2, 5, 7};
 
-	__int64 start = GetTickCount64();
-	int lotto = combination(45, 6);
-	__int64 end = GetTickCount64();
-
-	cout << end - start << endl;
+	int ret = 0;
+	for (int pos = 0; pos < seq.size(); pos++)
+	{
+		ret = max(ret, Lis(pos));
+	}
 }	
 
